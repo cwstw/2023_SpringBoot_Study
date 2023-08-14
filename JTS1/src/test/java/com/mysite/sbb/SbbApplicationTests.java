1a04e28c9cfa51989c5f9f86926025d86ee390ff
cwstw <cwstw@naver.com>
2023-08-14 21:48:40 +0900

    2023-08-14 점프투스프링부트 2장 학습 중

--- JTS1/src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ JTS1/src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,13 +1,44 @@
 package com.mysite.sbb;
 
-import org.junit.jupiter.api.Test;
+import com.mysite.sbb.entity.Answer;
+import com.mysite.sbb.entity.Question;
+import com.mysite.sbb.repository.AnswerRepository;
+import com.mysite.sbb.repository.QuestionRepository;
+import jakarta.transaction.Transactional;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class SbbApplicationTests {
 
+	@Autowired
+	private QuestionRepository questionRepository;
+
+	@Autowired
+	private AnswerRepository answerRepository;
+
+	@Transactional
 	@Test
-	void contextLoads() {
+	public void testJpa() {
+		Optional<Question> oq = this.questionRepository.findById(2);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+
+		List<Answer> aList = q.getAnswerList();
+
+		assertEquals(1, aList.size());
+		assertEquals("네, 자동으로 생성됩니다.", aList.get(0).getContent());
 	}
 
 }
