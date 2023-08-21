51a79f80eaf92961e8102ebc507ef6d67e730a0d
cwstw <cwstw@naver.com>
2023-08-21 23:02:37 +0900

    2023-08-21 점프투스프링부트 2장 학습 완료!

--- JTS1/src/main/java/com/mysite/sbb/controller/AnswerController.java
+++ JTS1/src/main/java/com/mysite/sbb/controller/AnswerController.java
@@ -1,11 +1,14 @@
 package com.mysite.sbb.controller;
 
 import com.mysite.sbb.entity.Question;
+import com.mysite.sbb.form.AnswerForm;
 import com.mysite.sbb.service.AnswerService;
 import com.mysite.sbb.service.QuestionService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,9 +26,13 @@ public class AnswerController {
     public String createAnswer(
             Model model,
             @PathVariable("id") Integer id,
-            @RequestParam String content){
+            @Valid AnswerForm answerForm, BindingResult result){
         Question question = this.questionService.getQuestion(id);
-        this.answerService.create(question,content);
+        if(result.hasErrors()){
+            model.addAttribute("question",question);
+            return "question_detail";
+        }
+        this.answerService.create(question,answerForm.getContent());
         return String.format("redirect:/question/detail/%s",id);
     }
 
--- JTS1/src/main/java/com/mysite/sbb/controller/QuestionController.java
+++ JTS1/src/main/java/com/mysite/sbb/controller/QuestionController.java
@@ -1,14 +1,16 @@
 package com.mysite.sbb.controller;
 
 import com.mysite.sbb.entity.Question;
+import com.mysite.sbb.form.AnswerForm;
+import com.mysite.sbb.form.QuestionForm;
 import com.mysite.sbb.service.QuestionService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.bind.BindResult;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,10 +29,26 @@ public class QuestionController {
     }
 
     @GetMapping("/detail/{id}")
-    public String detail(Model model, @PathVariable("id") Integer id) {
+    public String detail(Model model, @PathVariable("id") Integer id,
+                         AnswerForm answerForm) {
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question",question);
         return "question_detail";
     }
+
+    @GetMapping("/create")
+    public String questionCreate(QuestionForm questionForm){
+        return "question_form";
+    }
+
+    @PostMapping("/create")
+    public String questionCreate(@Valid QuestionForm questionForm,
+                                 BindingResult result){
+        if(result.hasErrors())
+            return "question_form";
+        this.questionService.create(questionForm.getSubject(),questionForm.getContent());
+        return "redirect:/question/list";
+    }
+
 
 }
