0b09f58c0337ecf05896b7d80f9c5bec785570a9
cwstw <cwstw@naver.com>
2023-08-30 20:27:46 +0900

    2023-08-30 점프투스프링부트 3장 학습 중 - 로그인/로그아웃 완료

--- JTS1/src/main/java/com/mysite/sbb/SecurityConfig.groovy
+++ JTS1/src/main/java/com/mysite/sbb/SecurityConfig.groovy
@@ -2,14 +2,17 @@ package com.mysite.sbb
 
 import jakarta.servlet.DispatcherType;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain
-import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter
+import org.springframework.security.web.util.matcher.AndRequestMatcher;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -30,6 +33,10 @@ public class SecurityConfig {
                 .formLogin((formLogin) -> formLogin
                     .loginPage("/user/login")
                     .defaultSuccessUrl("/"))
+                .logout((logout)->logout
+                    .logoutRequestMatcher(new AndRequestMatcher("/user/logout"))
+                    .logoutSuccessUrl("/")
+                    .invalidateHttpSession(true))
         ;
         return http.build();
     }
@@ -37,5 +44,10 @@ public class SecurityConfig {
     @Bean
     PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
