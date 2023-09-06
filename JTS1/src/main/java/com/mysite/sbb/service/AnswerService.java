4e778cebcb56840948b537423fa0a7dc9c779ee9
cwstw <cwstw@naver.com>
2023-09-06 19:38:21 +0900

    2023-09-06 점프투스프링부트 3장 학습 중 - 수정과 삭제 완료

--- JTS1/src/main/java/com/mysite/sbb/service/AnswerService.java
+++ JTS1/src/main/java/com/mysite/sbb/service/AnswerService.java
@@ -1,5 +1,6 @@
 package com.mysite.sbb.service;
 
+import com.mysite.sbb.DataNotFoundException;
 import com.mysite.sbb.entity.Answer;
 import com.mysite.sbb.entity.Question;
 import com.mysite.sbb.entity.SiteUser;
@@ -8,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -22,5 +24,24 @@ public class AnswerService {
         answer.setQuestion(question);//질문
         answer.setAuthor(author);//작성자
         this.answerRepository.save(answer);//답변 정보 저장
+    }
+
+    public Answer getAnswer(Integer id) {
+        Optional<Answer> answer = this.answerRepository.findById(id);
+        if (answer.isPresent()) {
+            return answer.get();
+        } else {
+            throw new DataNotFoundException("answer not found");
+        }
+    }
+
+    public void modify(Answer answer, String content) {
+        answer.setContent(content);
+        answer.setModifyDate(LocalDateTime.now());
+        this.answerRepository.save(answer);
+    }
+
+    public void delete(Answer answer) {
+        this.answerRepository.delete(answer);
     }
 }
